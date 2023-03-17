2c0598dd45665c7ca9291d91202e46651f01ca40
igor <igornotfound@gmail.com>
2023-03-17 01:45:22 +0100

    hooks + async action-creator

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,11 +1,28 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import {useTypedSelector} from "../hooks/useTypedSelector";
+import {useAppDispatch} from "../hooks/useAppDispatch";
+import {fetchUsers} from "../store/action-creator/user";
 
 const UserList: FC = () => {
-  const {users, error, loading} = useTypedSelector(state => state.user)
+  const { users, loading, error } = useTypedSelector(state => state.user);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+  if (error) {
+    return <h1>{error}</h1>
+  }
+
   return (
     <div>
-
+      {users.map(user =>
+        <div key={user.id}>{user.name}</div>
+      )}
     </div>
   );
 };
